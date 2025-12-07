d56af1842504851ea87cb18f63f838939b424836
Sarah Schwartz <156597896+sarah-schwartz@users.noreply.github.com>
2025-12-07 10:45:13 +0200

    Time: 0 ms (100%), Space: 133.2 MB (68.75%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         std::vector<int> less, equal, greater;
-        for(int num: nums){
-            if(num < pivot)
+        less.reserve(nums.size());
+        equal.reserve(nums.size());
+        greater.reserve(nums.size());
+
+        for (int num : nums) {
+            if (num < pivot)
                 less.push_back(num);
-            else if(num == pivot)
+            else if (num == pivot)
                 equal.push_back(num);
-            else 
+            else
                 greater.push_back(num);
         }
+
         vector<int> result;
-        result.reserve(nums.size()); 
+        result.reserve(nums.size());
 
         result.insert(result.end(), less.begin(), less.end());
         result.insert(result.end(), equal.begin(), equal.end());
         result.insert(result.end(), greater.begin(), greater.end());
+        
         return result;
     }
 };
